55ceadf455017b37c259961a81e7e23143526437
Rich Starkie <webmaster@neontigerweb.co.uk>
2016-06-20 18:33:21 +0100

    Update onwebchat.php

--- onwebchat.php
+++ onwebchat.php
@@ -19,7 +19,7 @@ class onWebChatPlugin extends Plugin
         }
 
         $this->enable([
-            
+            'onTwigSiteVariables' => ['onTwigSiteVariables', 0],
         ]);
     }
 
@@ -36,6 +36,11 @@ class onWebChatPlugin extends Plugin
      */
     public function onTwigSiteVariables()
     {
+        $type = strtolower($this->config->get('plugins.onwebchat.chatid'));
+        if (empty($type)){
+            throw new \InvalidArgumentException('A ChatID must be defined. At the moment it is empty. Without a ChatID you will not be able to connect to your Chat Box');
+        }
+        
         $twig->twig_vars['onwebchat'] = $twig->twig->render('partials/onwebchat.html.twig');
     }
 }
