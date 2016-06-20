cb1019a74e6bfba4d7ae92076ef0026494a572c2
Rich Starkie <rich@neontigeruk.com>
2016-06-20 20:50:35 +0100

    onwebchat.php

--- onwebchat.php
+++ onwebchat.php
@@ -19,10 +19,8 @@ class onWebChatPlugin extends Plugin
         }
 
         $this->enable([
-<<<<<<< Updated upstream
-=======
+
             'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
->>>>>>> Stashed changes
             'onTwigSiteVariables' => ['onTwigSiteVariables', 0],
         ]);
     }
@@ -40,22 +38,14 @@ class onWebChatPlugin extends Plugin
      */
     public function onTwigSiteVariables()
     {
-<<<<<<< Updated upstream
+       
         $type = strtolower($this->config->get('plugins.onwebchat.chatid'));
         if (empty($type)){
-            throw new \InvalidArgumentException('A ChatID must be defined. At the moment it is empty. Without a ChatID you will not be able to connect to your Chat Box');
-        }
-        
-=======
-
-        $type = strtolower($this->config->get('plugins.onwebchat.chatid'));
-        if (empty($type)){
-            throw new \InvalidArgumentException('The Chat ID value must be defined. At the moment it is empty. If you do not have a ChatID you can get it from your onWebChat Control Panel.');
+            throw new \InvalidArgumentException('The Chat ID value must be defined. At the moment it is empty. Without a ChatID you will not be able to connect to your Chat Box.  If you do not have a ChatID you can get it from your onWebChat Control Panel.');
         }
 
     $twig = $this->grav['twig'];
 
->>>>>>> Stashed changes
         $twig->twig_vars['onwebchat'] = $twig->twig->render('partials/onwebchat.html.twig');
     }
 }
