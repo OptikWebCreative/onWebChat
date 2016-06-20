1af4a6fce21b33f7ce54cedadff58c367381911f
Rich Starkie <rich@neontigeruk.com>
2016-06-20 12:08:52 +0100

    initial

---
+++ chat.php
@@ -0,0 +1,42 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+
+class onWebChatPlugin extends Plugin
+{
+    public static function getSubscribedEvents() {
+        return [
+            'onPluginsInitialized' => ['onPluginsInitialized', 0],
+        ];
+    }
+
+    public function onPluginsInitialized()
+    {
+        if ($this->isAdmin()) {
+            $this->active = false;
+            return;
+        }
+
+        $this->enable([
+            'onTwigTemplatePaths' => ['onTwigTemplatePaths', 0],
+            'onTwigSiteVariables' => ['onTwigSiteVariables', 0],
+        ]);
+    }
+
+    /**
+     * Add current directory to twig lookup paths.
+     */
+    public function onTwigTemplatePaths()
+    {
+        $this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
+    }
+
+    /**
+     * if enabled on this page, load the JS + CSS theme.
+     */
+    public function onTwigSiteVariables()
+    {
+        $twig->twig_vars['onwebchat'] = $twig->twig->render('partials/chat.html.twig');
+    }
+}
