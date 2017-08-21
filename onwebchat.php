fd5cfa0153a4acf9bed46f096e0cb1917a751e21
Hugh Barnes <dev@hubns.com>
2017-08-21 21:04:13 +1200

    Fix horrible mixed indentation (my bad) in PHP

--- onwebchat.php
+++ onwebchat.php
@@ -6,32 +6,32 @@ use Grav\Common\Plugin;
 
 class onWebChatPlugin extends Plugin
 {
-    public static function getSubscribedEvents() {
-        return [
-            'onPluginsInitialized' => ['onPluginsInitialized', 0]
-        ];
-    }
+	public static function getSubscribedEvents() {
+		return [
+			'onPluginsInitialized' => ['onPluginsInitialized', 0]
+		];
+	}
 
     public function onPluginsInitialized()
     {
 		$pluginsobject = (array) $this->config->get('plugins');
 		$enabled = (isset($pluginsobject['onwebchat']) && $pluginsobject['onwebchat']['enabled']);
 
-        if ($this->isAdmin() || !$enabled) {
-            $this->active = false;
-            return;
-        }
+		if ($this->isAdmin() || !$enabled) {
+			$this->active = false;
+			return;
+		}
 
 		$this->enable([
 			'onPageInitialized' => ['onPageInitialized', 0]
 		]);
     }
 
-    public function onPageInitialized()
-    {
-        $pageobject = $this->grav['page'];
+	public function onPageInitialized()
+	{
+		$pageobject = $this->grav['page'];
 
-        if(isset($pageobject->header()->onwebchat)) {
+		if(isset($pageobject->header()->onwebchat)) {
 			$activate = $pageobject->header()->onwebchat;
 		}
 		else {
@@ -41,5 +41,5 @@ class onWebChatPlugin extends Plugin
 		if ($activate) {
 			$this->grav['assets']->addInlineJs('var onWebChat={ar:[], set: function(a,b){if (typeof onWebChat_==="undefined"){this.ar.push([a,b]);}else{onWebChat_.set(a,b);}},get:function(a){return(onWebChat_.get(a));},w:(function(){ var ga=document.createElement("script"); ga.type = "text/javascript";ga.async=1;ga.src="//www.onwebchat.com/clientchat/'.$this->config->get('plugins.onwebchat.chatid').'";var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(ga,s);})()}');
 		}
-    }
+	}
 }
